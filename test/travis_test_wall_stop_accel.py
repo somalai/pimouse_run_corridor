a37c6a6278ae3c2950f01a97817d1aba9bcedc75
somalai <somalai@gmail.com>
2018-11-22 10:22:59 +0800

    Add WallStopAccel test

--- test/travis_test_wall_stop_accel.py
+++ test/travis_test_wall_stop_accel.py
@@ -3,7 +3,7 @@ import unittest, rostest
 import rosnode, rospy
 import time
 
-class WallStopTest(unittest.TestCase):
+class WallStopAccelTest(unittest.TestCase):
 	def set_and_get(self, lf, ls, rs, rf):
 		with open("/dev/rtlightsensor0", "w") as f:
 			f.write("%d %d %d %d\n" %(rf, rs, ls, lf))
@@ -20,13 +20,17 @@ class WallStopTest(unittest.TestCase):
 		left,right = self.set_and_get(400, 100, 100, 0) #total:600
 		self.assertTrue(left == 0 and right == 0, "can not stop")
 		
-		left,right = self.set_and_get(400, 0, 0, 99)	#total :499
-		self.assertTrue(left != 0 and right != 0, "can't move again")
+		left,right = self.set_and_get(40, 0, 0, 9)	#total :49
+		self.assertTrue(0 < left == right < 1000, "can't move again")
 		
-		left,right = self.set_and_get(150, 0, 200, 150) #total : 500
-		self.assertTrue(left == 0 and right == 0, "can not stop")
+		time.sleep(5.0)
+		left, right = self.set_and_get(40, 0, 0, 9) #total : 49
+		self.assertTrue(2000<left == right, "can't accelerate")
+
+		left,right = self.set_and_get(15, 0, 20, 15) #total : 50
+		self.assertTrue(left == right == 0, "can not stop")
 
 if __name__ == '__main__':
 	time.sleep(3)
-	rospy.init_node('travis_test_wall_stop')
-	rostest.rosrun('pimouse_run_corridor', 'travis_test_wall_stop', WallStopTest)
+	rospy.init_node('travis_test_wall_stop_accel')
+	rostest.rosrun('pimouse_run_corridor', 'travis_test_wall_stop', WallStopAccelTest)
